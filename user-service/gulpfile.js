ded8783d0208925038e6f08c79d2c0458d3342e9
Karl Wallenius <karl.wallenius@gmail.com>
2017-11-19 22:48:09 +0100

    Build files

---
+++ user-service/gulpfile.js
@@ -0,0 +1,56 @@
+import { spawn } from 'child_process';
+
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var gulpDocker = require('gulp-docker');
+var path = require('path');
+var del = require('del');
+var webpack = require('webpack');
+var webpackStream = require('webpack-stream');
+var webpackConfig = require('./webpack.config');
+
+gulp.task('clean', () => {
+  return del(['build/*']);
+});
+
+gulp.task('compile', ['run-tests'], () => {
+  gulp.src('./index.js')
+    .pipe(webpackStream(webpackConfig), webpack)
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('copy-package', ['compile'], () => {
+  gulp.src('./package.json')
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('run-tests', ['clean'], () => {
+  return gulp.src(['test/*.spec.js'], { read: false })
+    .pipe(mocha({
+      reporter: 'spec',
+      globals: {
+        should: require('should')
+      }
+    })).on('error', () => {
+      process.exit(1);
+    });
+});
+
+gulp.task('docker-build', ['copy-package'], () => {
+
+});
+
+gulp.task('build', ['clean', 'run-tests', 'compile', 'copy-package']);
+
+// Function run command line tools.
+function execCommand(command, args, callback) {
+  const ctx = spawn(command, args);
+
+  ctx.stdout.on('data', (data) => {
+    process.stdout.write(data);
+  });
+
+  ctx.on('close', (code) => {
+    if (callback) { callback(code === 0 ? null : code)}
+  });
+}
---
+++ user-service/webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+var path = require('path');
+var fs = require('fs');
+
+var nodeModules = {};
+
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+module.exports = {
+  entry: './index.js',
+  target: 'node',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'app.js'
+  },
+  externals: nodeModules,
+  plugins: [
+    new UglifyJsPlugin({
+      include: /js$/,
+      minimize: true
+    })
+  ],
+  devtool: 'sourcemap'
+}

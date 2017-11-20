26b6f9f2f94b3d64ac67aeaaddcbfb80753a3e98
Karl Wallenius <karl.wallenius@gmail.com>
2017-11-20 01:55:35 +0100

    Fixed error output and stops server at error

--- user-service/repository/db.js
+++ user-service/repository/db.js
@@ -9,11 +9,15 @@ module.exports.connect = (connectionSettings) => {
   var options = connectionSettings.options;
   
   mongoose.Promise = global.Promise;
-  mongoose.connect(uri, { useMongoClient: true });
+  mongoose.connect(uri, { useMongoClient: true }).catch(() => { console.log('App stopped.'); process.exit(1) });
 
   db = mongoose.connection;
 
-  db.on('error', console.error.bind(console, 'connection error:'));
+  //db.on('error', console.error.bind(console, 'Connection error:'));
+  db.on('error', (err) => {
+    console.error(err.message);
+  });
+
   db.once('open', () => {
     console.log('Connected to database.');
   });
